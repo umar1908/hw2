c37202aead337dc60ce16365af5ab1a6f37f9264
umar1908 <umar190801@gmail.com>
2021-02-18 21:47:33 +0300

    task2 main algorithm

--- task2.cpp
+++ task2.cpp
@@ -2,7 +2,7 @@
 #include <iomanip>
 using namespace std;
 // #include <functional>
-// #include <algorithm>
+#include <algorithm>
 #include <vector>
 // #include <string>
 // #include <cmath>
@@ -12,6 +12,10 @@ using namespace std;
 // #include <bitset>
 #include <chrono>
 #include <ctime>
+#include <cassert>
+#include <deque>
+#include <array>
+#include <list>
 
 class Timer{
 private:
@@ -42,15 +46,17 @@ private:
   chrono::steady_clock::duration sort_inside_result;
   chrono::steady_clock::duration sort_outside_result;
 public:
+
   Tester(int n){
     for (size_t i = 0; i < n; i++) {
       data.push_back(rand());
     }
   }
+
   template <typename T>
-  test(T& container){
+  void outside_test(T& container){
     auto start=begin(container);
-    auto end=end(container);
+    auto finish=end(container);
     assert (container.size()==data.size());
     for(auto elem:data){
       *start=elem;
@@ -59,9 +65,29 @@ public:
     timer.start();
     sort(begin(container),end(container));
     timer.stop();
-    cout<<"sort_outside_result="<<timer.get();
+    cout<<"sort_outside_result="<<timer.get()<<endl;
+
+  }
+  template<typename T>
+  void inside_test(T& container){
+    auto start=begin(container);
+    auto finish=end(container);
+    assert (container.size()==data.size());
+    for(auto elem:data){
+      *start=elem;
+      start++;
+    }
+    timer.start();
+    container.sort();
+    timer.stop();
+    cout<<"sort_inside_result="<<timer.get()<<endl;
+  }
 
 
+  void get_data()const{
+    for(auto elem: data){
+      cout<<elem<<" ";
+    }
   }
 
 };
@@ -71,5 +97,44 @@ int main(){
   system("chcp 1251"); //для корректного отображения кирилицы
   srand(static_cast<unsigned int>(time(0)));
   cout<<endl;
+  const int N=100000;
+
+
+
+
+
+
+
+
+  Tester tester1(N);
+
+  {
+    cout<<"vector\n";
+  vector<int> vec(N);
+  tester1.outside_test(vec);
+  // tester1.inside_test(vec);
+  }
+  {
+    cout<<"\ndeque\n";
+  deque<int> dec(N);
+  tester1.outside_test(dec);
+  // tester1.inside_test(dec);
+  }
+  {
+    cout<<"\narray\n";
+  std::array<int,N> arr;
+  tester1.outside_test(arr);
+  // tester1.inside_test(arr);
+  }
+  {
+    cout<<"\nlist\n";
+  list<int> lst(N);
+  // tester1.outside_test(arr);
+  tester1.inside_test(lst);
+  }
+
+
+
+
 
 }
